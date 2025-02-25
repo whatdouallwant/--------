995d63987306726cfd3629e822c77005faed1c33
Whynot <masya.babenko@gmail.com>
2025-02-25 17:53:19 +0200

    almostt

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db_scripts import dbManager
 app = Flask(__name__)
 db = dbManager("book.db")
 @app.route("/")
 def home():
-    book = db.get_book()
-    print("Отримані книги:", book)
-    return render_template("index.html", books=book)
+    search_query = request.args.get("search", "")
+    genre_filter = request.args.get("genre", "all")
 
+    books = db.get_books()
+    if search_query:
+        books = [book for book in books if search_query.lower() in book[0].lower() or search_query.lower() in book[1].lower()]
+    if genre_filter != "all":
+        books = [book for book in books if book[3].lower() == genre_filter.lower()]
+
+    return render_template("index.html", books=books, search_query=search_query, genre_filter=genre_filter)
 @app.route("/about")
 def about():
     return render_template("about.html")
--- db_scripts.py
+++ db_scripts.py
@@ -5,21 +5,18 @@ class dbManager():
         self.dbname = os.path.abspath(dbname) 
         self.conn = None
         self.cursor = None
-    def get_book(self):
-        self.conn = sqlite3.connect(self.dbname)
-        self.cursor = self.conn.cursor()
-        self.cursor.execute(''' SELECT * FROM book''')
-        data = self.cursor.fetchall()
-        self.conn.close()
-        return data
+
     def open_db(self):
         self.conn = sqlite3.connect(self.dbname) 
         self.cursor = self.conn.cursor() 
 
-    def search(self, query):
-        self.open_db()
-        query = '%' + query + '%'
-        self.cursor.execute( ''' SELECT * FROM book WHERE (name LIKE ? OR author LIKE ?)''', [query, query])
+    def get_books(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute("SELECT * FROM book")
         data = self.cursor.fetchall()
         self.conn.close()
-        return data
+        
+        clean_data = [tuple(str(item) for item in row) for row in data]
+        
+        return clean_data
